3ffa37f714dd4d62c7a0d9e6fee7d9f0e06bdc6c
0868307 <0868307@hr.nl>
2015-01-24 14:55:01 +0100

    dingetjes

---
+++ src/main/java/Controller.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Wouter on 1/24/2015.
+ */
+import com.google.gson.*;
+import datastructuur.Queue;
+import pojo.Klant;
+import pojo.Pojo;
+
+import java.io.*;
+
+public class Controller {
+    public static final String JSON_KLANT = "./data/klant.json";
+    public static final String JSON_BESTELLING = "./data/bestelling.json";
+    FileReader file;
+    public static void main(String[] args) {
+        new Controller();
+    }
+
+    public Controller() {
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/pojo/Pojo.java
@@ -0,0 +1,8 @@
+package pojo;
+
+/**
+ * Created by Wouter on 1/24/2015.
+ */
+public abstract class Pojo {
+
+}
